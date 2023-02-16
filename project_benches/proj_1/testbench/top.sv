43a1dfe87e2dd42439a73cd988bd2bc87fbe350e
Santosh Srivatsan <srsrivat@ncsu.edu>
2023-02-16 00:18:34 -0500

    provide_read_data() works yayyy

--- project_benches/proj_1/testbench/top.sv
+++ project_benches/proj_1/testbench/top.sv
@@ -1,9 +1,16 @@
 `timescale 1ns / 10ps
 
-`define CSR_ADDR 	2'h0
+`define CSR_ADDR	2'h0
 `define DPR_ADDR 	2'h1
 `define CMDR_ADDR 	2'h2
 
+`define CMD_START     8'bxxxx_x100
+`define CMD_STOP      8'bxxxx_x101
+`define CMD_READ_ACK  8'bxxxx_x010
+`define CMD_READ_NACK 8'bxxxx_x011
+`define CMD_WRITE     8'bxxxx_x001
+`define CMD_SET_BUS   8'bxxxx_x110
+`define CMD_WAIT      8'bxxxx_x000
 
 module top();
 
@@ -35,8 +42,8 @@ assign sda = addr ? 'b0 : 'bz;
 // Clock generator
 
 initial begin: clk_gen
-   clk = 1'b0;
-   forever #5 clk = ~clk;
+	 clk = 1'b0;
+	 forever #5 clk = ~clk;
 end
 
 // ****************************************************************************
@@ -45,7 +52,7 @@ end
 // Active high reset (pg. 14, iicmb_mb spec)
 
 initial begin: rst_gen
-  #113 rst = 1'b0;
+	#113 rst = 1'b0;
 end
 
 // ****************************************************************************
@@ -56,7 +63,7 @@ logic [WB_DATA_WIDTH-1:0] mon_data;
 logic mon_we;
 
 initial begin: wb_monitoring
-  $timeformat(-9, 2, " ns", 12);
+	$timeformat(-9, 2, " ns", 12);
 	forever begin
 		#10 wb_bus.master_monitor (.addr(mon_addr), .data(mon_data), .we(mon_we));
 		if (mon_we) begin
@@ -79,150 +86,206 @@ end
 // ****************************************************************************
 // Define the flow of the simulation
 
-logic [WB_DATA_WIDTH-1:0] cmdr_rdata;
+logic [WB_DATA_WIDTH-1:0] 	cmdr_rdata;
+logic [WB_DATA_WIDTH-1:0] 	dpr_rdata;
+logic 						transfer_complete;
 
 initial begin: test_flow
 
-    // 6.1. Example 1 (pg. 22): Enable the IICMB core after power up
-    // by writing the 'E' bit of the CSR register. Also enable interrupts (for Example 3)
-    wait (!rst)
-    wb_bus.master_write (.addr(`CSR_ADDR), .data('b11xx_xxxx)); 
+	// 6.1. Example 1 (pg. 22): Enable the IICMB core after power up
+	// by writing the 'E' bit of the CSR register. Also enable interrupts (for Example 3)
+	wait (!rst)
+	wb_bus.master_write (.addr(`CSR_ADDR), .data('b11xx_xxxx)); 
 
-    // 6.3. Example 3: Write a byte 0x78 to a slave with address 0x22 
-    // residing on IIC bus #5
+	// 6.3. Example 3: Write a byte 0x78 to a slave with address 0x22 
+	// residing on IIC bus #5
 
-    // 6.3.1. Write byte 0x05 to the DPR. This is the ID of the desired IIC bus
-    wb_bus.master_write (.addr(`DPR_ADDR), .data('h00));
+	// 6.3.1. Write byte 0x00 to the DPR. This is the ID of the desired IIC bus
+	wb_bus.master_write (.addr(`DPR_ADDR), .data('h0a));
 
-    // 6.3.2. Write byte 'xxxx_x110' to the CMDR. This is the `Set Bus` command (pg. 7)
-    wb_bus.master_write (.addr(`CMDR_ADDR), .data('bxxxx_x110));
+	// 6.3.2. Write byte 'xxxx_x110' to the CMDR. This is the `Set Bus` command (pg. 7)
+	wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_SET_BUS));
 
-    // 6.3.3. Wait for interrupt or until the DON bit of the CMDR reads '1'
-    wait (irq);
-    wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata)); // Clear interrupt output
-    // This command fails since the DUT isn't connected to an I2C slave resulting in 
-    // protocol errors
+	// 6.3.3. Wait for interrupt or until the DON bit of the CMDR reads '1'
+	wait (irq);
+	wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata)); // Clear interrupt output
 
-    // 6.3.4. Write byte 'xxxx_x100' to the CMDR. This is the start command (pg. 7)
-    wb_bus.master_write (.addr(`CMDR_ADDR), .data('bxxxx_x100));
+	// 6.3.4. Write byte 'xxxx_x100' to the CMDR. This is the start command (pg. 7)
+	wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_START));
 
-    // 6.3.5. Wait for interrupt or until the DON bit of the CMDR reads '1'
-    wait (irq);
-    wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata)); // Clear interrupt output
+	// 6.3.5. Wait for interrupt or until the DON bit of the CMDR reads '1'
+	wait (irq);
+	wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
 
-    // 6.3.6. Write byte 0x44 to the DPR. This is the slave address 0x22 shifted 
-    // 1 bit to the left + rightmost bit = '0', which means writing
-    wb_bus.master_write (.addr(`DPR_ADDR), .data ('h22 << 1));
+	// 6.3.6. Write byte 0x44 to the DPR. This is the slave address 0x22 shifted 
+	// 1 bit to the left + rightmost bit = '0', which means writing
+	wb_bus.master_write (.addr(`DPR_ADDR), .data ('h22 << 1));
 
-    // 6.3.7. Write byte 'xxxx_x001' to the CMDR. This is the Write command (pg. 7)
-    wb_bus.master_write (.addr(`CMDR_ADDR), .data ('bxxxx_x001));
+	// 6.3.7. Write byte 'xxxx_x001' to the CMDR. This is the Write command (pg. 7)
+	wb_bus.master_write (.addr(`CMDR_ADDR), .data (`CMD_WRITE));
 
-    // 6.3.8. Wait for interrupt or until the DON bit of the CMDR reads '1'. If instead
-    // of DON, the NAK bit is '1', then slave doesn't respond
+	// 6.3.8. Wait for interrupt or until the DON bit of the CMDR reads '1'. If instead
+	// of DON, the NAK bit is '1', then slave doesn't respond
+	wait (irq);
+	wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
 
-    wait (irq);
-    wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata)); // Clear interrupt output
+	// 6.3.9. Write byte 0x78 to the DPR. This is the byte to be written
+	wb_bus.master_write (.addr(`DPR_ADDR), .data ('h78));
 
-    // 6.3.9. Write byte 0x78 to the DPR. This is the byte to be written
-    wb_bus.master_write (.addr(`DPR_ADDR), .data ('h78));
+	// 6.3.10. Write byte 'xxxx_x001' to the CMDR. This is the Write command (pg. 7)
+	wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_WRITE));
 
-    // 6.3.10. Write byte 'xxxx_x001' to the CMDR. This is the Write command (pg. 7)
-    wb_bus.master_write (.addr(`CMDR_ADDR), .data('bxxxx_x001));
+	// 6.3.11. Wait for interrupt or until the DON bit of the CMDR reads '1'.
+	wait (irq);
+	wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
 
-    // 6.3.11. Wait for interrupt or until the DON bit of the CMDR reads '1'.
-    wait (irq);
-    wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata)); // Clear interrupt output
+	// 6.3.12. Write byte 'xxxx_x101' to the CMDR. This is the Stop command
+	wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_STOP));
 
-    // 6.3.12. Write byte 'xxxx_x101' to the CMDR. This is the Stop command
-    wb_bus.master_write (.addr(`CMDR_ADDR), .data('bxxxx_x101));
+	// 6.3.13. Wait for interrupt or until the DON bit of the CMDR reads '1'.
+	wait (irq);
+	wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
 
-    // 6.3.13. Wait for interrupt or until the DON bit of the CMDR reads '1'.
-    wait (irq);
-    wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata)); // Clear interrupt output
+	////////////////////////////////////////////////////////////////////////////
 
-    $finish;
+	// 6.5. Example 5: Read byte from slave address 0x44 and memory location 0xAA
+
+	/* 4 */ wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_START));
+
+	/* 5 */ wait (irq); 
+			wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
+
+	/* 6 */ wb_bus.master_write (.addr(`DPR_ADDR), .data('h44 << 1)); // Write slave address
+
+	/* 7 */ wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_WRITE));
+
+	/* 8 */ wait (irq); 
+			wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
+
+	/* 9 */ wb_bus.master_write (.addr(	`DPR_ADDR), .data ('hAA)); // Write memory location
+
+	/* 10 */ wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_WRITE));
+
+	/* 11 */ wait (irq); 
+			 wb_bus.master_read (.addr(`CMDR_ADDR), .data (cmdr_rdata));
+
+	/* 12 */ wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_START)); // Repeated START. Does it work without this??
+
+	/* 13 */ wait (irq); 
+			 wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
+
+	/* 14 */ wb_bus.master_write (.addr(`DPR_ADDR), .data('h89));
+
+	/* 15 */ wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_WRITE));
+
+	/* 16 */ wait (irq);
+			 wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
+
+	/* 17 */ wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_READ_NACK));
+
+	// Call provide_read_data here
+			 i2c_bus.provide_read_data (.read_data(8'hab), .transfer_complete(transfer_complete));
+			 wait (transfer_complete);
+
+	/* 18 */ wait (irq);
+			 wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
+
+	/* 19 */ wb_bus.master_read (.addr(`DPR_ADDR), .data(dpr_rdata));
+
+	/* 20 */ wb_bus.master_write (.addr(`CMDR_ADDR), .data(`CMD_STOP));
+
+	/* 21 */ wait (irq);
+			 wb_bus.master_read (.addr(`CMDR_ADDR), .data(cmdr_rdata));
+
+		$finish;
 end
 
+bit op;
+bit [I2C_DATA_WIDTH-1:0] write_data;
 
 initial begin: i2c_flow
 
-forever begin
-	i2c_bus.wait_for_i2c_transfer ();
-end
+	// Wait for reset because a STOP condition occurs at 0ns
+	wait (!rst);
+
+	forever begin
+			$display ("================================================");
+			i2c_bus.wait_for_i2c_transfer (.op(op), .write_data(write_data));
+	end
+
 end
 
 // ****************************************************************************
 // Instantiate the Wishbone master Bus Functional Model
 wb_if #(
-  .ADDR_WIDTH(WB_ADDR_WIDTH),
-  .DATA_WIDTH(WB_DATA_WIDTH)
+	.ADDR_WIDTH(WB_ADDR_WIDTH),
+	.DATA_WIDTH(WB_DATA_WIDTH)  
 )
 wb_bus (
-  // System sigals
-  .clk_i(clk),
-  .rst_i(rst),
-  // Master signals
-  .cyc_o(cyc),
-  .stb_o(stb),
-  .ack_i(ack),
-  .adr_o(adr),
-  .we_o(we),
-  // Slave signals
-  .cyc_i(),
-  .stb_i(),
-  .ack_o(),
-  .adr_i(),
-  .we_i(),
-  // Shred signals
-  .dat_o(dat_wr_o),
-  .dat_i(dat_rd_i)
-  );
+	// System sigals
+	.clk_i(clk),
+	.rst_i(rst),
+	// Master signals
+	.cyc_o(cyc),
+	.stb_o(stb),
+	.ack_i(ack),
+	.adr_o(adr),
+	.we_o(we),
+	// Slave signals
+	.cyc_i(),
+	.stb_i(),
+	.ack_o(),
+	.adr_i(),
+	.we_i(),
+	// Shred signals
+	.dat_o(dat_wr_o),
+	.dat_i(dat_rd_i)
+	);
 
 // ****************************************************************************
 // Instantiate the I2C slave Bus Functional Model
 i2c_if #(
-  .NUM_BUSSES(NUM_I2C_BUSSES),
-  .ADDR_WIDTH(I2C_ADDR_WIDTH),
-  .DATA_WIDTH(I2C_DATA_WIDTH)
+	.NUM_BUSSES(NUM_I2C_BUSSES),
+	.ADDR_WIDTH(I2C_ADDR_WIDTH),
+	.DATA_WIDTH(I2C_DATA_WIDTH)
 )
 i2c_bus (
-  // Master signals
-  .scl_o (scl),
-  .sda_o (sda),
-  // Slave signals
-  .scl_i (scl),
-  .sda_i (sda)
+	// Master signals
+	.sda_o (sda),
+	// Slave signals
+	.scl_i (scl),
+	.sda_i (sda)
 );
 
 // ****************************************************************************
 // Instantiate the DUT - I2C Multi-Bus Controller
 \work.iicmb_m_wb(str) #(.g_bus_num(NUM_I2C_BUSSES)) DUT
-  (
-    // ------------------------------------
-    // -- Wishbone signals:
-    .clk_i(clk),         // in    std_logic;                            -- Clock
-    .rst_i(rst),         // in    std_logic;                            -- Synchronous reset (active high)
-    // -------------
-    .cyc_i(cyc),         // in    std_logic;                            -- Valid bus cycle indication
-    .stb_i(stb),         // in    std_logic;                            -- Slave selection
-    .ack_o(ack),         //   out std_logic;                            -- Acknowledge output
-    .adr_i(adr),         // in    std_logic_vector(1 downto 0);         -- Low bits of Wishbone address
-    .we_i(we),           // in    std_logic;                            -- Write enable
-    .dat_i(dat_wr_o),    // in    std_logic_vector(7 downto 0);         -- Data input
-    .dat_o(dat_rd_i),    //   out std_logic_vector(7 downto 0);         -- Data output
-    // ------------------------------------
-    // ------------------------------------
-    // -- Interrupt request:
-    .irq(irq),           //   out std_logic;                            -- Interrupt request
-    // ------------------------------------
-    // ------------------------------------
-    // -- I2C interfaces:
-    .scl_i(scl),         // in    std_logic_vector(0 to g_bus_num - 1); -- I2C Clock inputs
-    .sda_i(sda),         // in    std_logic_vector(0 to g_bus_num - 1); -- I2C Data inputs
-    .scl_o(scl),         //   out std_logic_vector(0 to g_bus_num - 1); -- I2C Clock outputs
-    .sda_o(sda)          //   out std_logic_vector(0 to g_bus_num - 1)  -- I2C Data outputs
-    // ------------------------------------
-  );
+	(
+		// ------------------------------------
+		// -- Wishbone signals:
+		.clk_i(clk),         // in    std_logic;                            -- Clock
+		.rst_i(rst),         // in    std_logic;                            -- Synchronous reset (active high)
+		// -------------
+		.cyc_i(cyc),         // in    std_logic;                            -- Valid bus cycle indication
+		.stb_i(stb),         // in    std_logic;                            -- Slave selection
+		.ack_o(ack),         //   out std_logic;                            -- Acknowledge output
+		.adr_i(adr),         // in    std_logic_vector(1 downto 0);         -- Low bits of Wishbone address
+		.we_i(we),           // in    std_logic;                            -- Write enable
+		.dat_i(dat_wr_o),    // in    std_logic_vector(7 downto 0);         -- Data input
+		.dat_o(dat_rd_i),    //   out std_logic_vector(7 downto 0);         -- Data output
+		// ------------------------------------
+		// ------------------------------------
+		// -- Interrupt request:
+		.irq(irq),           //   out std_logic;                            -- Interrupt request
+		// ------------------------------------
+		// ------------------------------------
+		// -- I2C interfaces:
+		.scl_i(scl),         // in    std_logic_vector(0 to g_bus_num - 1); -- I2C Clock inputs
+		.sda_i(sda),         // in    std_logic_vector(0 to g_bus_num - 1); -- I2C Data inputs
+		.scl_o(scl),         //   out std_logic_vector(0 to g_bus_num - 1); -- I2C Clock outputs
+		.sda_o(sda)          //   out std_logic_vector(0 to g_bus_num - 1)  -- I2C Data outputs
+		// ------------------------------------
+	);
 
 
 endmodule
--- verification_ip/interface_packages/i2c_pkg/src/i2c_if.sv
+++ verification_ip/interface_packages/i2c_pkg/src/i2c_if.sv
@@ -1,15 +1,14 @@
 // ECE 745: Project 1
 // I2C Interface for an IICMB controller
 
-typedef enum bit [1:0] {WRITE=2'b00, READ=2'b01, NULL=2'b11} i2c_op_t;
+typedef enum bit {WRITE=1'b0, READ=1'b1} i2c_op_t;
 
 interface i2c_if #(
     int NUM_BUSSES = 1,
-    int ADDR_WIDTH = 32,
-    int DATA_WIDTH = 16
+    int ADDR_WIDTH = 7,
+    int DATA_WIDTH = 8
 )(
     // Outputs from slave
-    output wire [NUM_BUSSES-1:0] scl_o,
     output wire [NUM_BUSSES-1:0] sda_o,
 
     // Inputs to slave (come from master)
@@ -17,153 +16,231 @@ interface i2c_if #(
     input wire [NUM_BUSSES-1:0] sda_i
 );
 
+// TODO: I'm assuming that DATA_WIDTH is a multiple of 8
+localparam NUM_BYTES = DATA_WIDTH / 8;
 
-// Declare queue for capturing stuff on the sda line
-bit         transfer_q[$];
-bit         i2c_start = 1'b0;
-bit [7:0]   i2c_addr;
-i2c_op_t    i2c_op;
-byte        i2c_data; 
-bit         i2c_ack = 1'b0;
+static bit                  do_ack   = 1'b0;
+static bit                  wren     = 1'b0;  
+static bit [NUM_BUSSES-1:0] wdata;
 
-assign sda_o = i2c_ack ? 'b0 : 'bz;
+// assign sda_o = do_ack ? 'b0 : (wren ? wdata : 'bz);
+
+assign sda_o = do_ack ? 'b0 : 'bz;
+assign sda_o = wren   ? wdata : 'bz;
+
+// assign sda_o = drive_sda_high ? 'b1 : 'bz;
+// assign sda_o = drive_sda_low  ? wdata : 'bz;
 
 //////////////////////////////////////////////////////////
 
-// Functions to read from a queue
+// Functions to read data from a queue and then flush the queue
 
-function bit read_op ();
+function automatic bit read_op_from_q (ref bit q[$]);
 
-    {<< bit {read_op}} = transfer_q;
-    transfer_q.delete();
-
+    {<< bit {read_op_from_q}} = q;
+    q.delete();
 endfunction
 
-function bit [6:0] read_addr ();
+function automatic bit [6:0] read_addr_from_q (ref bit q[$]);
 
-    {<< 7 {read_addr}} = transfer_q;
-    transfer_q.delete();
-
+    {<< 7 {read_addr_from_q}} = q;
+    q.delete();
 endfunction
 
-function byte read_data ();
+function automatic bit [DATA_WIDTH-1:0] read_data_from_q (ref bit q[$]);
 
-    {<< byte {read_data}} = transfer_q;
-    transfer_q.delete();
-
+    {<< DATA_WIDTH {read_data_from_q}} = q;
+    q.delete();
 endfunction
 
+// Functions to store data into a queue
+
+function automatic void write_data_to_q (input bit [DATA_WIDTH-1:0] data, ref bit q[$]);
+
+    {>> {q}} = data;
+endfunction
 
 ////////////////////////////////////////////////////////////////////////////
 
-// START: sda goes low while scl is high
-// Spawn thread to repeatedly check for a start condition
-// TODO: Thread should resume to check for repeated start
+// START: A HIGH to LOW transition on the SDA line while SCL is HIGH
 
-task capture_start (output bit is_start);
+task automatic capture_start (ref bit busy);
 
-    fork
-    begin: loop
-        forever begin
-            @(negedge sda_i);
-            if (scl_i) begin
-                is_start = 1'b1;
-                $display ("Found a start!");
+    forever begin
+        @(negedge sda_i);
+        if (scl_i) begin
+            // A repeated START occurs if we get a START while the bus is busy
+            $display ("I2C START condition.");
+            if (busy)
                 break;
-            end else begin
-                is_start = 1'b0;       
-            end 
+            else
+                busy = 1'b1;
         end
     end
-    join_any
 endtask
 
 ////////////////////////////////////////////////////////////////////////////
 
-task capture_bit ();
+// STOP: A LOW to HIGH transition on the SDA line while SCL is HIGH
+
+task automatic capture_stop(ref bit busy);
+
+    forever begin
+        @(posedge sda_i);
+        if (scl_i) begin
+            $display ("I2C STOP condition");
+            busy = 1'b0;
+            break;
+        end
+    end
+endtask
+
+////////////////////////////////////////////////////////////////////////////
+
+// Tasks to capture data on the sda line into a transfer queue
+
+task automatic capture_bit (ref bit q[$]);
 
     @(posedge scl_i);
-    transfer_q.push_back(sda_i);
+    q.push_back(sda_i);
+    // Wait until we're sure we didn't see a repeated START/STOP condition
+    @(negedge scl_i); 
 endtask
 
 ////////////////////////////////////////////////////////////////////////////
 
-
-task capture_addr (output bit [7:0] addr);
-
-    repeat (7) capture_bit();
-    addr = read_addr ();
-    $display ("I2C Address = 0x%x", addr);
-
-endtask
-
-////////////////////////////////////////////////////////////////////////////
-
-task capture_op (output i2c_op_t op);
-
-    capture_bit();
-    $cast(op, read_op());
-    $display ("I2C Op = %s", op.name);
-
-endtask
-
-////////////////////////////////////////////////////////////////////////////
-
-
-task capture_data (output byte data);
-
-    repeat (8) capture_bit();
-    data = read_data ();
-    $display ("I2C Data = 0x%x", data);
-
-endtask
-
-////////////////////////////////////////////////////////////////////////////
+// ACK: slave pulls SDA low. Remains low for the HIGH period of the clock
 
 task send_ack ();
 
+    do_ack = 1'b1;
     @(negedge scl_i);
-    i2c_ack = 1'b1;
-    @(negedge scl_i);
-    i2c_ack = 1'b0;
-
+    do_ack = 1'b0;
 endtask;
 
+// ACK: master pulls SDA low. Remains low for the HIGH period of the clock
 
-////////////////////////////////////////////////////////////////////////////
+task wait_for_ack();
 
-task wait_for_i2c_transfer ();
-
-    capture_start (.is_start(i2c_start));
-
-    wait (i2c_start);
-
-    capture_addr (.addr(i2c_addr));
-    capture_op   (.op(i2c_op));
-    send_ack     ();
-
-    // TODO: We need to check for a repeated start here. Because the transfer 
-    // of data could end anytime
-    capture_data (.data(i2c_data));
-    send_ack     ();
-
-    // TODO: Write task to capture stop
-
+    wait (!sda_i);
+    @(posedge scl_i);
+    @(negedge scl_i);
 endtask;
 
 ////////////////////////////////////////////////////////////////////////////
 
-// Provide data for read operation
+// Thread #1: Detect START and repeated START conditions
+// Thread #2: Capture data once a START condition has been detected
+// Thread #3: Detect a STOP condition and terminate data transfer
+
+task wait_for_i2c_transfer (
+    output i2c_op_t op,
+    output bit [DATA_WIDTH-1:0] write_data
+);
+
+    // automatic - initialized every time the task is called
+    // Queue for capturing data on the SDA line
+    automatic   bit         q[$]                ;
+    automatic   bit [7:0]   device_addr         ;
+    static      bit         bus_busy    = 1'b0  ;   // static variable. Preserve across calls              
+
+    fork
+
+    // Capture START and repeated START conditions (sets the bus_busy flag)
+    begin: DETECT_START
+        capture_start(.busy(bus_busy));
+    end
+
+    begin: DETECT_TRANSFER
+
+        // Wait for a START condition (detected by capture_start)
+        wait (bus_busy);
+
+        // Capture the slave address into a queue
+        repeat(7) capture_bit(q);
+        device_addr = read_addr_from_q (q);
+        $display ("I2C Address = 0x%X", device_addr);
+
+        // Capture the I2C operation (R/W)
+        capture_bit(q);
+        $cast(op, read_op_from_q(q));
+        $display ("I2C Operation = %s", op.name);
+
+        // Acknowledge the first byte
+        send_ack();
+
+        if (op == WRITE) begin
+
+            // Capture data into a queue
+            for (int i = 0; i < NUM_BYTES; i++) begin
+                repeat(8) capture_bit(q);
+                send_ack(); 
+            end
+            write_data = read_data_from_q (q);
+            $display ("I2C Data = 0x%X", write_data);
+        end
+        
+        if (op == READ) begin
+
+            // Wait for `provide_read_data()` to to write NUM_BYTES on the sda line
+            for (int i = 0; i < NUM_BYTES; i++) begin
+                repeat(8) @(posedge scl_i);
+            end
+        end
+
+        // TODO: Capture a NACK before STOP condition. Especially if the transfer
+        //       direction is going to change. See Fig 12 and Notes on pg. 14
+
+        // Wait for a STOP condition to free the bus
+        wait (!bus_busy);
+    end
+
+    // Capture a STOP condition (resets the bus_busy flag)
+    begin: DETECT_STOP
+        capture_stop(.busy(bus_busy));
+    end
+    join_any
+
+    // Kill all threads, return to testbench
+    disable fork;
+endtask;
+
+////////////////////////////////////////////////////////////////////////////
+
+// TODO: I'm not sure whether this implements read with ack or read with nack
+// TODO: I'm not sure whether this task is supposed to drive the SDA line or not
+//       I'm driving the SDA line
+
 task provide_read_data (
     input bit [DATA_WIDTH-1:0] read_data, 
     output bit transfer_complete
 );
+    // Queue for pushing data onto the SDA line
+    automatic bit q[$];
 
+    write_data_to_q(read_data, q);
+
+    for (int i = 0; i < NUM_BYTES; i++) begin
+
+        repeat (8) begin
+            // Allow the slave to drive the SDA line (MSB first)
+            wren = 1'b1; 
+            wdata = q.pop_front();
+            @(posedge scl_i);
+            @(negedge scl_i);
+        end
+    end
+
+    // Release the SDA line
+    wren = 1'b0; 
+    transfer_complete = 1'b1;
 endtask
 
 ////////////////////////////////////////////////////////////////////////////
 
 // Return data observed
+// TODO: You have to implement the entire logic of the protocol in monitor
+// Shouldn't be dependent on wait for i2c transfer
 task monitor (
     output bit [ADDR_WIDTH-1:0] addr, 
     output i2c_op_t op, 
