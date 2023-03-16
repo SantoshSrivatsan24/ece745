233afacabeda525b5c9a8c211ff4b662749a6e0e
Santosh Srivatsan <srsrivat@ncsu.edu>
2023-03-16 18:54:48 -0400

    Refactor predictor

--- verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
+++ verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
@@ -7,12 +7,6 @@ typedef enum bit [2:0] {
     STATE_READ_BYTE
 } bus_state_t;
 
-typedef enum bit [1:0] {
-    STATE_WAIT_START_STOP,
-    STATE_CAPTURE_ADDR,
-    STATE_CAPTURE_DATA
-} i2c_state_t;
-
 typedef enum bit [2:0] {
     CMD_START       = 3'b100,
     CMD_STOP        = 3'b101,
@@ -22,6 +16,12 @@ typedef enum bit [2:0] {
     CMD_SET_BUS     = 3'b110,
     CMD_WAIT        = 3'b000
 } cmd_t;
+
+typedef enum bit [1:0] {
+    CSR_ADDR = 2'h0,
+    DPR_ADDR = 2'h1,
+    CMDR_ADDR = 2'h2
+} addr_t;
 
 typedef struct packed {
     bit don;
@@ -34,7 +34,7 @@ typedef struct packed {
 
 typedef union packed {
     byte value;
-    cmdr_t fields;
+    cmdr_t cmdr;
 } cmdr_u;
 
 class i2cmb_predictor extends ncsu_component #(.T(wb_transaction));
@@ -45,15 +45,11 @@ class i2cmb_predictor extends ncsu_component #(.T(wb_transaction));
     local bus_state_t current_bus_state;
     local bus_state_t next_bus_state;
 
-    local i2c_state_t current_i2c_state;
-    local i2c_state_t next_i2c_state;
-
+    local bit addr_complete;
     local bit [7:0] dpr;
-    local bit got_addr;
     local i2c_op_t i2c_op;
     local bit [6:0] i2c_addr;
     local bit [7:0] i2c_data[$];
-    local bit transfer_complete;
 
     function new (string name = "", ncsu_component_base parent = null);
         super.new(name, parent);
@@ -62,10 +58,11 @@ class i2cmb_predictor extends ncsu_component #(.T(wb_transaction));
     endfunction
 
     virtual function void nb_put (input T trans);
+        bit transfer_complete;
         this.wb_trans = trans;
-        this.run_golden_model();
-        if (this.transfer_complete) begin
-            this.transfer_complete = 1'b0;
+        transfer_complete = this.run_golden_model();
+        if (transfer_complete) begin
+            this.addr_complete = 1'b0;
             this.i2c_trans.op = this.i2c_op;
             this.i2c_trans.addr = this.i2c_addr;
             {>> 8 {this.i2c_trans.data}} = this.i2c_data;
@@ -74,125 +71,110 @@ class i2cmb_predictor extends ncsu_component #(.T(wb_transaction));
         end
     endfunction
 
-    // TODO: This function should modify i2c trans based on wb trans
-    // I have to basically implement the entire i2c protocol in a 
-    // non blocking fashion.
-    // The predictor models the DUT. It incrementally constructs
-    // an i2c transaction
-    local function void run_golden_model();
-        cmdr_u cmdr_value;
-        cmdr_t cmdr;
+    // The predictor models the byte-level FSM in the DUT. 
+    // It incrementally constructs an i2c transaction
+    local function bit run_golden_model();
+        addr_t addr = addr_t'(this.wb_trans.addr);
+        cmdr_u data = this.wb_trans.data;
+        bit we      = this.wb_trans.we;
+        cmdr_t cmdr = data.cmdr;
+        bit transfer_complete = 1'b0;
 
-        // Read the DPR register
-        if (this.wb_trans.addr == 2'h1) begin
-            this.dpr = this.wb_trans.data;
-            if (this.wb_trans.we == 1'b0) begin
-                i2c_data.push_back(dpr);
+        case (current_bus_state)
+        STATE_IDLE: begin
+            if(addr == CMDR_ADDR && cmdr.cmd == CMD_START) begin
+                next_bus_state = STATE_START;
+            end 
+            else if(addr == CMDR_ADDR && cmdr.cmd == CMD_SET_BUS) begin
+                next_bus_state = STATE_IDLE; 
+            end 
+            else begin
+                $error ("Invalid command");
             end
         end
 
-        // Read the CMDR register
-        else if (this.wb_trans.addr == 2'h2) begin
-            cmdr_value  = this.wb_trans.data;
-            cmdr        = cmdr_value.fields;
-
-            case (current_bus_state)
-            STATE_IDLE: begin
-                if(cmdr.cmd == CMD_START) begin
-                    next_bus_state = STATE_START;
-                end 
-                else if(cmdr.cmd == CMD_SET_BUS) begin
-                    next_bus_state = STATE_IDLE; 
-                end 
-                else begin
-                    $error ("Invalid command");
-                end
+        STATE_START: begin
+            if(cmdr.don) begin
+                next_bus_state = STATE_BUSY;
+                if (this.addr_complete) // Repeated START
+                    transfer_complete = 1'b1;
             end
-
-            STATE_START: begin
-                // Successful START condition
-                if(cmdr.don) begin
-                    next_bus_state = STATE_BUSY;
-                    if (this.got_addr) this.transfer_complete = 1'b1;
-                    this.got_addr = 1'b0;
-                end
-                // Unsuccessful START condition
-                else if (cmdr.err || cmdr.al) begin
-                    next_bus_state = STATE_IDLE;
-                end
-                else begin
-                    $error ("Invalid command");
-                end
+            else if (cmdr.err || cmdr.al) begin
+                next_bus_state = STATE_IDLE;
             end
-
-            STATE_BUSY: begin
-                if (cmdr.cmd == CMD_WRITE) begin
-                    next_bus_state = STATE_WRITE_BYTE;
-                end
-                else if (cmdr.cmd == CMD_READ_ACK || cmdr.cmd == CMD_READ_NAK) begin
-                    next_bus_state = STATE_READ_BYTE;
-                end 
-                else if (cmdr.cmd == CMD_START) begin
-                    next_bus_state = STATE_START;
-                end
-                else if (cmdr.cmd == CMD_STOP) begin
-                    next_bus_state = STATE_STOP;
-                end
-                else begin
-                    $error ("Invalid command");
-                end
+            else begin
+                $error ("Invalid flag");
             end
-
-            STATE_WRITE_BYTE: begin
-                if(cmdr.don || cmdr.nak) begin
-                    next_bus_state = STATE_BUSY;
-                    if (!this.got_addr) begin
-                        this.got_addr   = 1'b1;
-                        this.i2c_op     = i2c_op_t'(dpr[0]);
-                        this.i2c_addr   = this.dpr[7:1];
-                    end else begin
-                        this.i2c_data.push_back(dpr);
-                    end
-                end
-                else if (cmdr.err || cmdr.al) begin
-                    next_bus_state = STATE_IDLE;
-                end
-                else begin
-                    $error ("Invalid command");
-                end
-            end
-
-            STATE_READ_BYTE: begin
-                if (cmdr.don) begin
-                    next_bus_state = STATE_BUSY;
-                end
-                else if (cmdr.err || cmdr.al) begin
-                    next_bus_state = STATE_IDLE;
-                end
-                else begin
-                    $error ("Invalid command");
-                end
-            end
-
-            STATE_STOP: begin
-                if (cmdr.don) begin
-                    next_bus_state = STATE_IDLE;
-                    this.transfer_complete = 1'b1;
-                    this.got_addr = 1'b0;
-                end
-                else begin
-                    $error ("Invalid command");
-                end
-            end
-
-            default: $error ("Invalid state");
-            endcase
-
-            // $display ("Current bus state: %s", current_bus_state.name);
-            // $display ("Next bus state: %s", next_bus_state.name);
-            current_bus_state = next_bus_state;
         end
+
+        STATE_BUSY: begin
+            if (addr == DPR_ADDR) begin
+                this.dpr = data.value;
+                if (!we) // Read data from the BFM is stored in the DPR
+                    this.i2c_data.push_back(this.dpr);
+            end
+            else if (addr == CMDR_ADDR && cmdr.cmd == CMD_WRITE) begin
+                next_bus_state = STATE_WRITE_BYTE;
+            end
+            else if (addr == CMDR_ADDR && (cmdr.cmd == CMD_READ_ACK || cmdr.cmd == CMD_READ_NAK)) begin
+                next_bus_state = STATE_READ_BYTE;
+            end 
+            else if (addr == CMDR_ADDR && cmdr.cmd == CMD_START) begin
+                next_bus_state = STATE_START;
+            end
+            else if (addr == CMDR_ADDR && cmdr.cmd == CMD_STOP) begin
+                next_bus_state = STATE_STOP;
+            end
+            else begin
+                $error ("Invalid command");
+            end
+        end
+
+        STATE_WRITE_BYTE: begin
+            if(cmdr.don || cmdr.nak) begin
+                next_bus_state = STATE_BUSY;
+                if (!this.addr_complete) begin
+                    this.addr_complete = 1'b1;
+                    this.i2c_op = i2c_op_t'(dpr[0]);
+                    this.i2c_addr = this.dpr[7:1];
+                end else begin
+                    this.i2c_data.push_back(this.dpr);
+                end
+            end
+            else if (cmdr.err || cmdr.al) begin
+                next_bus_state = STATE_IDLE;
+            end
+            else begin
+                $error ("Invalid flag");
+            end
+        end
+
+        STATE_READ_BYTE: begin
+            if (cmdr.don) begin
+                next_bus_state = STATE_BUSY;
+            end
+            else if (cmdr.err || cmdr.al) begin
+                next_bus_state = STATE_IDLE;
+            end
+            else begin
+                $error ("Invalid command");
+            end
+        end
+
+        STATE_STOP: begin
+            if (cmdr.don) begin
+                next_bus_state = STATE_IDLE;
+                transfer_complete = 1'b1;
+            end
+            else begin
+                $error ("Invalid command");
+            end
+        end
+
+        default: $error ("Invalid state");
+        endcase
+
+        current_bus_state = next_bus_state;
+        return transfer_complete;
     endfunction
-
-
 endclass
